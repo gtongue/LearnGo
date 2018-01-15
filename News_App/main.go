3c9e0eb4d7aacc24795c5fab1a4cee3c642c9374
Garrett Tongue <garretttongue@Garretts-MacBook-Pro.local>
2018-01-15 13:24:42 -0800

    Fetch data from wall street journal

--- News_App/main.go
+++ News_App/main.go
@@ -10,23 +10,64 @@ import (
 const sitemap string = "https://www.washingtonpost.com/news-sitemap-index.xml"
 
 type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+type NewsMap struct {
+	Keyword  string
+	Location string
 }
+
+// type Location struct {
+// 	Loc string `xml:"loc"`
+// }
+// func (l Location) String() string {
+// 	return fmt.Sprintf(l.Loc)
+// }
 
 func main() {
+	var s SitemapIndex
+	var n News
+	news_map := make(map[string]NewsMap)
+
 	resp, _ := http.Get(sitemap)
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-
-	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
-	fmt.Println(s.Locations)
+	// fmt.Println(s.Locations)
+	// for i := 0; i < 10; i++ {
+	// }
+
+	// range returns two values the index and the value
+	for _, Location := range s.Locations {
+		// fmt.Println(Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	for idx, data := range news_map {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
+	}
 }
+
+// Using maps
+// mapping := make(map[string]float32)
+// mapping["Garrett"] = 44
+// mapping["Test"] = 90
+// delete(mapping, "Test")
+// for k, v := range mapping {
+
+// }
